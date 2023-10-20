567ea79ddf9310e6e42a94605d2a33b19fef96c4
Ahsan <ahsanul.kabir@bjitgroup.com>
2023-10-20 17:33:59 +0600

    add a post request

--- Sources/App/Controllers/SongController.swift
+++ Sources/App/Controllers/SongController.swift
@@ -11,11 +11,17 @@ import Vapor
 struct SongController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let songs = routes.grouped("songs")
+        songs.post(use: create)
         songs.get(use:index)
     }
     
     //songs route
     func index(req: Request) throws -> EventLoopFuture<[Song]> {
         return Song.query(on: req.db).all()
+    }
+
+    func create(req:Request) throws -> EventLoopFuture<HTTPStatus> {
+        let song = try req.content.decode(Song.self)
+        return song.save(on: req.db).transform(to: .ok)
     }
 }
